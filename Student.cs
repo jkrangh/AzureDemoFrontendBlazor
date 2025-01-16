e5e4b2602e5bb693ad909239c96e5ea367f552f0
Johan <147413295+jkrangh@users.noreply.github.com>
2025-01-16 15:24:32 +0100

    Add project files.

---
+++ Student.cs
@@ -0,0 +1,11 @@
+﻿namespace AzureDemoFrontendBlazor
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BiggestFear { get; set; }
+
+    }
+}
